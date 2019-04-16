3807df0365eaf8afff36722c8d56516b03e2e4ff
Nicolas Marini <nmarini44@gmail.com>
2019-04-16 23:28:24 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -37,6 +37,6 @@ describe('Fix the Scope', function() {
   describe('attemptTwoFavoriteCustomers', function(){
     // it('unsuccessfully tries to declare favoriteCustomer with let two times', () => {
     //   expect(attemptTwoFavoriteCustomers).to.throw(SyntaxError)
-    // }) -- Told by instructor to comment out 
+    // }) -- Told by instructor to comment out
   })
 })
