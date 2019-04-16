dd512b38391e8c1782f4ad94642843058a97993c
Nicolas Marini <nmarini44@gmail.com>
2019-04-16 22:55:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function changeLeastFavoriteCustomer() {
 
 function attemptTwoFavoriteCustomers(){
     let favoriteCustomer = 'bar'
-     favoriteCustomer = 'not bar'
+    var favoriteCustomer = 'not bar'
 }
